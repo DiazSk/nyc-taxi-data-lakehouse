46c2b5310a6323e4ef6883419d0e2fcfb8314284
DiazSk <zaid07sk@gmail.com>
2026-02-16 13:00:48 -0800

    feat: Update dbt staging models and expand data quality tests with accepted ranges

--- nyc_taxi_analytics/models/staging/stg_taxi_trips.sql
+++ nyc_taxi_analytics/models/staging/stg_taxi_trips.sql
@@ -4,6 +4,8 @@
     )
 }}
 
+{% set s3_bucket = var('s3_bucket', 'nyc-taxi-data-lake-ygzcn2t2') %}
+
 SELECT
     tpep_pickup_datetime,
     tpep_dropoff_datetime,
@@ -13,6 +15,6 @@ SELECT
     avg_speed_mph,
     year,
     month
-FROM read_parquet('s3://nyc-taxi-data-lake-ygzcn2t2/processed-data/**/*.parquet')
+FROM read_parquet('s3://{{ s3_bucket }}/processed-data/**/*.parquet')
 WHERE trip_duration_minutes > 0
   AND trip_distance > 0
